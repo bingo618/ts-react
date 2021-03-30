54b2e00c84e75f935d332758d510149984e1c4e9
hubin <bin9850@163.com>
2021-03-30 16:00:37 +0800

    ✨feat(全局): 第一次提交

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+const OFF = 0
+const WARN = 1
+const ERROR = 2
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      ERROR,
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        json: 'never',
+        js: 'never',
+      },
+    ],
+  },
+}
